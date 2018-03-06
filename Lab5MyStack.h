794a61883989d20a687c28257ac02d679c4ad924
braxtonparnell <35747653+braxtonparnell@users.noreply.github.com>
2018-03-05 18:36:48 -0700

    Update Lab5MyStack.h

--- Lab5MyStack.h
+++ Lab5MyStack.h
@@ -1,13 +1,24 @@
 //MyStack function declarations
+// Braxton Parnell
+// COSC 2030
+
+#pragma once
 #include<string>
+#include <stack>
+#include <vector>
+#include <list>
 
 std::string stringReversal1(std::string input);
 std::string stringReversal2(std::string input);
 std::string stringReversal3(std::string input);
 std::string stringReversal4(std::string input);
 
-class MyStack{
-  bool isEmpty() const;
-  void push(char& c);
-  char pull();
+class MyStack {
+	public:
+	bool isEmpty() const;
+	void push(char& c);
+	char pull();
+
+private:
+	std::vector<char> myvec;
 };
