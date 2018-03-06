f04121f46018d6792b19ebe697f7ff08a53b94ad
braxtonparnell <35747653+braxtonparnell@users.noreply.github.com>
2018-03-05 18:36:18 -0700

    Update Lab5Driver.cpp

--- Lab5Driver.cpp
+++ Lab5Driver.cpp
@@ -1,10 +1,28 @@
+// Lab5Driver.cpp : Defines the entry point for the console application.
+// Braxton Parnell
+// COSC 2030
+
+#include "stdafx.h"
 //Lab7Driver
 #include "Lab5MyStack.h"//change this to .cpp if on mac/linux
 //any other includes here
+#include "iostream"
+#include<string>
 
-int main(){
-  //TODO: implement and test your functions here
+using std::cout;
+using std::endl;
 
-  //system("pause");
-  return 0;
+int main() {
+	//TODO: implement and test your functions here
+
+	//system("pause");
+
+	std::string test ("stop");
+
+	cout << "Reverse 'stop'using stringReversal1: " << stringReversal1(test) << endl;
+	cout << "Reverse 'stop'using stringReversal2: " << stringReversal2(test) << endl;
+	cout << "Reverse 'stop'using stringReversal3: " << stringReversal3(test) << endl;
+	cout << "Reverse 'stop'using stringReversal4: " << stringReversal4(test) << endl;
+
+	return 0;
 }
