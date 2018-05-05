bd29993ad47b0a1f8c87f1abbe0acceecdbb30ca
braxtonparnell <35747653+braxtonparnell@users.noreply.github.com>
2018-05-04 23:37:45 -0600

    Update Proj2.cpp

--- Proj2.cpp
+++ Proj2.cpp
@@ -9,10 +9,13 @@ using std::vector;
 using std::cout;
 using std::endl;
 
-void print(vector<int>);
+void printvec(vector<int>);
+void printarr(int[], int);
+
 void quicksort(vector<int>&, int, int);
 int binarysearch(vector<int>, int, int, int);
-//vector<int> mergesort(vector<int>);
+void mergesort(int [], int, int);
+void merge(int [], int l, int m, int r);
 //int hash(int);
 
 
@@ -25,21 +28,45 @@ int main()
 	vector1.push_back(10);
 	vector1.push_back(9);
 
-	print(vector1);
+	cout << "Vector: ";
+	printvec(vector1);
 
+	cout << "Using quicksort on vector. ";
 	quicksort(vector1, 0, vector1.size() - 1);
 
-	print(vector1);
+	cout << "Sorted vector using quicksort: ";
+	printvec(vector1);
 
-	cout << "4 in vector1? " << binarysearch(vector1, 0, vector1.size() - 1, 10) << endl;
-	//cout << "28 in vector1? " << binarysearch(vector1, 0, vector1.size(), 28) << endl;
-	//cout << "9 in vector1? " << binarysearch(vector1, 0, vector1.size(), 2) << endl;
+	cout << "Location of 8 in vector: (-1 if not in vector)" << binarysearch(vector1, 0, vector1.size() - 1, 8) << endl;
+	cout << "Location of 0 in vector: (-1 if not in vector)" << binarysearch(vector1, 0, vector1.size(), 0) << endl;
+	cout << "Location of -5 in vector: (-1 if not in vector)" << binarysearch(vector1, 0, vector1.size(), -5) << endl;
+
+	int arr[] = { 5, 29, 46, 12, 0, 7, 5 };
+
+	cout << "Array: ";
+	printarr(arr, 7);
+
+	cout << "Using mergesort on array. ";
+	mergesort(arr, 0, 7 - 1);
+
+	cout << "Sorted array using mergesort: ";
+	printarr(arr, 7);
+
 
 
     return 0;
 }
 
-void print(vector<int> vec) {
+void printarr(int arr[], int size) {
+	cout << "[";
+	for (int i = 0; i < size; i++) {
+		cout << arr[i] << ", ";
+	}
+	cout << "]";
+}
+
+
+void printvec(vector<int> vec) {
 	cout << "[";
 	for (unsigned int i = 0; i < vec.size(); i++) {
 		cout << vec.at(i) << ", ";
@@ -91,17 +118,73 @@ int binarysearch(vector<int> vec,int l, int r, int s) {
 	return -1;
 }
 
-/*
-vector<int> mergesort(vector<int> vec) {
-	//if (vec.size() > 1) {
-	//	vector<int> l, r;
 
+void mergesort(int arr[], int l, int r) {
+	if (l < r) {
+		int m = l + (r - 1) / 2;
 
-	//}
+		mergesort(arr, l, m);
+		mergesort(arr, m + 1, r);
 
-	return vector<int>;
+		merge(arr, l, m, r);
+	}
+	
 }
 
+void merge(int arr[], int l, int m, int r) {
+	int i, j, k;
+	int n1 = m - l + 1;
+	int n2 = r - m;
+
+	/* create temp arrays */
+	int L[7], R[7];
+
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++)
+		L[i] = arr[l + i];
+	for (j = 0; j < n2; j++)
+		R[j] = arr[m + 1 + j];
+
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray
+	j = 0; // Initial index of second subarray
+	k = l; // Initial index of merged subarray
+	while (i < n1 && j < n2)
+	{
+		if (L[i] <= R[j])
+		{
+			arr[k] = L[i];
+			i++;
+		}
+		else
+		{
+			arr[k] = R[j];
+			j++;
+		}
+		k++;
+	}
+
+	/* Copy the remaining elements of L[], if there
+	are any */
+	while (i < n1)
+	{
+		arr[k] = L[i];
+		i++;
+		k++;
+	}
+
+	/* Copy the remaining elements of R[], if there
+	are any */
+	while (j < n2)
+	{
+		arr[k] = R[j];
+		j++;
+		k++;
+	}
+}
+
+
+/*
 int hash(int s) {
 
 	return 0;
